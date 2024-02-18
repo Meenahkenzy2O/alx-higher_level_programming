31b53569b7e4b1eebab85593df1cce7e1a3c7b33
meenahkenz <itzamina65@gmail.com>
2024-02-18 06:27:56 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    name = dir(hidden_4)
-    for i in range(len(name)):
-        if name[i][0] != "_" and name[i][1] != "_":
-            print(name[i])
+    for name in dir(hidden_4):
+        if name[0] != '_' and name[1] != '_':
+            print(name)
